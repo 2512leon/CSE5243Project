a61f8596a08593cbb28148fd8d3fdb32820f88a9
Noah Woods <nwoods@Noahs-Air.attlocal.net>
2022-04-01 00:18:52 -0400

    finished creating dictionary for one sgm file

--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
+from typing import OrderedDict
 from bs4 import BeautifulSoup
+import collections
 
 fileNames = []
 topics = []
 places = []
 
-placesList=[]
-topicsList=[]
-
+placesList = []
+topicsList = []
 # for file in os.listdir("/data"):
 # if file.endswith(".sgm"):
 # filename = os.path.join("data", file)
@@ -17,16 +18,47 @@ soup = BeautifulSoup(dataRead, 'html.parser')
 places = soup.findAll('places')
 topics = soup.findAll('topics')
 
-for place in places:
-    placesList = soup.findAll('d')
+for places in soup.find_all('places'):
+    if len(places.text) == 0:
+        placesList.append('{}')
+    for place in places.find_all('d'):
+        placesList.append(place.text)
 
-for topic in topics:
-    topicsList = soup.findAll('d')
+for topics in soup.find_all('topics'):
+    if len(topics.text) == 0:
+        topicsList.append('{}')
+    for topic in topics.find_all('d'):
+        topicsList.append(topic.text)
 
+# list comprehension to convert lists into dictionaries with counts
+placesDictionary = {place:placesList.count(place) for place in placesList} # yay list comprehension
+topicsDictionary = {topic:topicsList.count(topic) for topic in topicsList}
 
-print(placesList)
-print("Topics:")
-print(topicsList)
+# for sorting the dictionaries and retaining some form of order since python dicts are unordered
+orderedPlaces = OrderedDict(sorted(placesDictionary.items()))
+orderedTopics = OrderedDict(sorted(topicsDictionary.items()))
 
+placesDictFinal = OrderedDict()
+topicsDictFinal = OrderedDict() 
 
+sequenceNumber = 1
+for k,v in orderedPlaces.items():
+    placesDictFinal[sequenceNumber] = [k,v]
+    sequenceNumber = sequenceNumber + 1
 
+sequenceNumber = 1
+for k,v in orderedTopics.items():
+    topicsDictFinal[sequenceNumber] = [k,v]
+    sequenceNumber = sequenceNumber + 1
+
+print("Places Dictionary:")
+for k,v in placesDictFinal.items():
+    print("Sequence Number:", k)
+    print("Place:",v[0])
+    print("Frequency:",v[1])
+
+print("\n\nTopics Dictionary: ")
+for k,v in topicsDictFinal.items():
+    print("Sequence Number:", k)
+    print("Topic:",v[0])
+    print("Frequency:",v[1])
